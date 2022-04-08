afbf2845e6c8ca37ac5155d3d9b2f7796d0becbe
ironmask431 <ironmask431@gmail.com>
2022-04-09 04:35:33 +0900

    first

---
+++ src/test/java/com/example/springbootjpacategory/SpringbootJpaCategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpacategory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaCategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
